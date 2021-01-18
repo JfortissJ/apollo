da92a20dcca6ff1cdfddd830bac8e823f24768f0
Tobias Kessler <kessler@fortiss.org>
2021-01-18 11:24:44 +0100

    integrated miqp planner c api

---
+++ miqp/include/src/miqp_planner_c_api.h
@@ -0,0 +1,33 @@
+// Copyright (c) 2020 Tobias Kessler, Klemens Esterle
+//
+// This work is licensed under the terms of the MIT license.
+// For a copy, see <https://opensource.org/licenses/MIT>.
+
+// we need to wrap: in this order
+// Trajectory traj = planner.GetBarkTrajectory(idx, t);
+//   MiqpPlanner planner = MiqpPlanner(settings, envPoly);
+// planner_.UpdateConvexifiedMap(envPoly_);
+// planner.AddObstacle(predictedTraj, obstacleShape);
+
+typedef void* CMiqpPlanner;
+
+extern "C" CMiqpPlanner newCMiqpPlanner();
+
+extern "C" void DelCMiqpPlanner(CMiqpPlanner c_miqp_planner);
+
+extern "C" int AddCarCMiqpPlanner(CMiqpPlanner c_miqp_planner,
+                                  double initial_state_in[], double ref_in[],
+                                  const int ref_size, double vDes,
+                                  const double timestep);
+
+extern "C" bool PlanCMiqpPlanner(CMiqpPlanner c_miqp_planner,
+                                 const double timestep);
+
+extern "C" void UpdateCarCMiqpPlanner(CMiqpPlanner c_miqp_planner, int idx,
+                                      double initial_state_in[],
+                                      double ref_in[], const int ref_size,
+                                      const double timestep);
+
+extern "C" void GetCTrajectoryCMiqpPlanner(CMiqpPlanner c_miqp_planner, int idx,
+                                           double start_time,
+                                           double* trajectory, int& size);
---
+++ miqp/miqp.BUILD
@@ -0,0 +1,9 @@
+package(default_visibility = ["//visibility:public"])
+
+cc_library(
+    name = "miqp_c_api",
+    copts = [],
+    includes = ["include/."],
+    srcs = ["lib/libmiqp_planner_c_api.so"],
+    visibility = ["//visibility:public"],
+)
