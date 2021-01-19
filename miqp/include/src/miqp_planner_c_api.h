352f9c7c09ca691b4023a3a1e927fcfeec7d98dd
Tobias Kessler <kessler@fortiss.org>
2021-01-19 14:22:22 +0100

    cleanup cplex integration

--- miqp/include/src/miqp_planner_c_api.h
+++ miqp/include/src/miqp_planner_c_api.h
@@ -4,8 +4,7 @@
 // For a copy, see <https://opensource.org/licenses/MIT>.
 
 // we need to wrap: in this order
-// Trajectory traj = planner.GetBarkTrajectory(idx, t);
-//   MiqpPlanner planner = MiqpPlanner(settings, envPoly);
+// MiqpPlanner planner = MiqpPlanner(settings, envPoly);
 // planner_.UpdateConvexifiedMap(envPoly_);
 // planner.AddObstacle(predictedTraj, obstacleShape);
 
