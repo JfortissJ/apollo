1676ade7673428980e2c38be862e9f306af6c485
Tobias Kessler <kessler@fortiss.org>
2021-03-08 17:32:13 +0100

    write miqp planner logs to folders

--- modules/planning/planner/miqp/miqp_planner.cc
+++ modules/planning/planner/miqp/miqp_planner.cc
@@ -28,6 +28,7 @@
 
 #include "cyber/common/log.h"
 #include "cyber/common/macros.h"
+#include "cyber/logger/logger_util.h"
 #include "modules/common/math/cartesian_frenet_conversion.h"
 #include "modules/common/math/path_matcher.h"
 #include "modules/common/time/time.h"
@@ -118,8 +119,27 @@ common::Status MiqpPlanner::Init(const PlanningConfig& config) {
   egoCarIdx_ = -1;                       // set invalid
   minimum_valid_speed_planning_ = 1.0;   // below our model is invalid
   standstill_velocity_threshold_ = 0.1;  // set velocity hard to zero below this
-  ActivateDebugFileWriteCMiqpPlanner(planner_, "/apollo/data/log",
-                                     "miqp_planner_");
+
+  // from cyber/logger/log_file_object.cc
+  struct ::tm tm_time;
+  const time_t timestamp = static_cast<time_t>(Clock::NowInSeconds());
+  localtime_r(&timestamp, &tm_time);
+  std::ostringstream time_pid_stream;
+  time_pid_stream.fill('0');
+  time_pid_stream << 1900 + tm_time.tm_year << std::setw(2)
+                  << 1 + tm_time.tm_mon << std::setw(2) << tm_time.tm_mday
+                  << '-' << std::setw(2) << tm_time.tm_hour << std::setw(2)
+                  << tm_time.tm_min << std::setw(2) << tm_time.tm_sec << '.'
+                  << apollo::cyber::logger::GetMainThreadPid();
+  const std::string& time_pid_string = time_pid_stream.str();
+  std::string logdir("/apollo/data/log/");
+  logdir += time_pid_string;
+  char logdir_cstr[logdir.length()];
+  strcpy(logdir_cstr, logdir.c_str());
+  char name_prefix_cstr[14];
+  strcpy(name_prefix_cstr, "miqp_planner_");
+  ActivateDebugFileWriteCMiqpPlanner(planner_, logdir_cstr, name_prefix_cstr);
+
   config_ = config;
   if (!config_.has_miqp_planner_config()) {
     AERROR << "Please provide miqp planner parameter file! " +
