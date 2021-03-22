34c3c00f511205d288f9c620797c9fa889508f24
klemense1 <esterle@fortiss.org>
2021-03-22 14:26:57 +0100

    working version of FakeObstacleComponent - TimerComponent - using reference line to create obstacle relative to ego vehicle

--- modules/fake_obstacle/fake_obstacle_component.cc
+++ modules/fake_obstacle/fake_obstacle_component.cc
@@ -18,7 +18,6 @@
 
 #include "modules/common/adapters/adapter_gflags.h"
 #include "modules/common/vehicle_state/vehicle_state_provider.h"
-#include "modules/fake_obstacle/proto/fake_obstacle_conf.pb.h"
 
 namespace apollo {
 namespace fake_obstacle {
@@ -28,6 +27,7 @@ using apollo::canbus::Chassis;
 using apollo::common::Status;
 using apollo::common::VehicleState;
 using apollo::common::VehicleStateProvider;
+using apollo::common::time::Clock;
 using apollo::hdmap::HDMapUtil;
 using apollo::localization::LocalizationEstimate;
 using apollo::perception::PerceptionObstacles;
@@ -35,8 +35,11 @@ using apollo::planning::ReferenceLineProvider;
 using apollo::routing::RoutingResponse;
 
 FakeObstacleComponent::FakeObstacleComponent()
-    : monitor_logger_buffer_(common::monitor::MonitorMessageItem::PLANNING) {
-  AERROR << "Started fake obstacle node!";
+    : monitor_logger_buffer_(common::monitor::MonitorMessageItem::PLANNING),
+      s_init_obstacle_(0.0),
+      t_init_obstacle_(0.0),
+      obstacle_has_started_(false) {
+  AERROR << "Started FakeObstacleComponent !";
 }
 
 FakeObstacleComponent::~FakeObstacleComponent() {
@@ -47,8 +50,7 @@ FakeObstacleComponent::~FakeObstacleComponent() {
 
 bool FakeObstacleComponent::Init() {
   // load proto file
-  FakeObstacleConf fake_obst_config;
-  if (!cyber::common::GetProtoFromFile(config_file_path_, &fake_obst_config)) {
+  if (!cyber::common::GetProtoFromFile(config_file_path_, &fake_obst_config_)) {
     monitor_logger_buffer_.ERROR("Unable to load fake obstacle conf file: " +
                                  config_file_path_);
   }
@@ -64,34 +66,42 @@ bool FakeObstacleComponent::Init() {
 
   // load map
   hdmap_ = HDMapUtil::BaseMapPtr();
-  CHECK(hdmap_) << "Failed to load map";
+  CHECK(hdmap_) << "Failed to load map file:" << apollo::hdmap::BaseMapFile();
 
   // instantiate reference line provider
-  // reference_line_provider_ = std::make_unique<ReferenceLineProvider>(hdmap_);
-  // // does not build, maybe some compiler version issue?
-  reference_line_provider_ =
-      std::unique_ptr<ReferenceLineProvider>(new ReferenceLineProvider(hdmap_));
+  reference_line_provider_ = std::make_unique<ReferenceLineProvider>(hdmap_);
   reference_line_provider_->Start();
+
+  AERROR << "Initialized FakeObstacleComponent !";
 
   return true;
 }
 
 bool FakeObstacleComponent::InitReaders() {
-  // routing_reader_ = node_->CreateReader<RoutingResponse>(
-  //     FLAGS_routing_response_topic,
-  //     [this](const std::shared_ptr<RoutingResponse>& routing) {
-  //       AINFO << "Received routing data: run routing callback."
-  //             << routing->header().DebugString();
-  //       std::lock_guard<std::mutex> lock(mutex_);
-  //       latest_routing_.CopyFrom(*routing);
-  //     });
+  routing_reader_ = node_->CreateReader<RoutingResponse>(
+      FLAGS_routing_response_topic,
+      [this](const std::shared_ptr<RoutingResponse>& routing) {
+        AINFO << "Received routing data: run routing callback."
+              << routing->header().DebugString();
+        std::lock_guard<std::mutex> lock(mutex_);
+        latest_routing_.CopyFrom(*routing);
+      });
+  if (routing_reader_ == nullptr) {
+    AERROR << "Create routing response reader failed";
+    return false;
+  }
 
   chassis_reader_ = node_->CreateReader<Chassis>(
       FLAGS_chassis_topic, [this](const std::shared_ptr<Chassis>& chassis) {
-        ADEBUG << "Received chassis data: run chassis callback.";
+        ADEBUG << "Received chassis data: run chassis callback."
+               << chassis->header().DebugString();
         std::lock_guard<std::mutex> lock(mutex_);
         latest_chassis_.CopyFrom(*chassis);
       });
+  if (chassis_reader_ == nullptr) {
+    AERROR << "Create chassis reader failed";
+    return false;
+  }
 
   localization_reader_ = node_->CreateReader<LocalizationEstimate>(
       FLAGS_localization_topic,
@@ -101,52 +111,44 @@ bool FakeObstacleComponent::InitReaders() {
         std::lock_guard<std::mutex> lock(mutex_);
         latest_localization_.CopyFrom(*localization);
       });
+  if (localization_reader_ == nullptr) {
+    AERROR << "Create localization reader failed";
+    return false;
+  }
 
   return true;
 }
 
-bool FakeObstacleComponent::Proc(
-    const std::shared_ptr<RoutingResponse>& routing) {
-  bool success = UpdateReferenceLine(routing);
-
-  auto response = std::make_shared<PerceptionObstacles>();
-  // TODO: fill response
-  apollo::perception::PerceptionObstacle* ob =
-      response->add_perception_obstacle();
-
-  apollo::common::Point3D position;
-  position.set_x(latest_localization_.pose().position().x() + 5);
-  position.set_y(latest_localization_.pose().position().y() + 5);
-  position.set_z(latest_localization_.pose().position().z());
-  ob->mutable_position()->CopyFrom(position);
-
-  // optional double theta = 3;  // heading in the world coordinate system.
-  ob->set_theta(latest_localization_.pose().heading());
-
-  // TODO: fill velocity
-  // optional apollo.common.Point3D velocity = 4;  // obstacle velocity.
-
-  // Size of obstacle bounding box.
-  ob->set_length(4);  // obstacle length.
-  ob->set_width(2);   // obstacle width.
-  ob->set_height(2);  // obstacle height.
-
-  obstacle_writer_->Write(response);
-
+bool FakeObstacleComponent::Proc() {
+  FillPerceptionObstacles();
   return true;
 }
 
-bool FakeObstacleComponent::UpdateReferenceLine(
-    const std::shared_ptr<routing::RoutingResponse>& routing) {
-  ADEBUG << "Get localization:" << latest_localization_.DebugString();
-  ADEBUG << "Get chassis:" << latest_chassis_.DebugString();
+bool FakeObstacleComponent::UpdateReferenceLine() {
+  AINFO << "Get routing:" << latest_routing_.DebugString();
+  if (!latest_routing_.has_header()) {
+    AERROR << "routing not received yet";
+    return false;
+  }
+
+  AINFO << "Get localization:" << latest_localization_.DebugString();
+  if (!latest_localization_.has_header()) {
+    AERROR << "localization not received yet";
+    return false;
+  }
+
+  AINFO << "Get chassis:" << latest_chassis_.DebugString();
+  if (!latest_chassis_.has_header()) {
+    AERROR << "chassis not received yet";
+    return false;
+  }
 
   Status status = VehicleStateProvider::Instance()->Update(latest_localization_,
                                                            latest_chassis_);
   VehicleState vehicle_state =
       VehicleStateProvider::Instance()->vehicle_state();
 
-  reference_line_provider_->UpdateRoutingResponse(*routing);
+  reference_line_provider_->UpdateRoutingResponse(latest_routing_);
   reference_line_provider_->UpdateVehicleState(vehicle_state);
 
   std::list<hdmap::RouteSegments> segments;
@@ -155,6 +157,65 @@ bool FakeObstacleComponent::UpdateReferenceLine(
     AERROR << "Failed to create reference line";
     return false;
   }
+
+  DCHECK_EQ(last_reference_lines_.size(), segments.size());
+
+  return true;
+}
+
+bool FakeObstacleComponent::FillPerceptionObstacles() {
+  if (!obstacle_has_started_) {
+    if (!UpdateReferenceLine()) {
+      AERROR << "Could not update reference";
+      return false;
+    }
+
+    common::math::Vec2d ego_vec2d =
+        common::math::Vec2d(latest_localization_.pose().position().x(),
+                            latest_localization_.pose().position().y());
+    common::SLPoint ego_sl;
+    last_reference_lines_.front().XYToSL(ego_vec2d, &ego_sl);
+
+    s_init_obstacle_ = ego_sl.s() + fake_obst_config_.delta_s_in_front();
+    t_init_obstacle_ = Clock::NowInSeconds();
+    obstacle_has_started_ = true;
+  }
+
+  double diff_time = Clock::NowInSeconds() - t_init_obstacle_;
+  double s_current_obstacle =
+      s_init_obstacle_ + diff_time * fake_obst_config_.velocity();
+  planning::ReferencePoint other_ref_point =
+      last_reference_lines_.front().GetReferencePoint(s_current_obstacle);
+
+  auto response = std::make_shared<PerceptionObstacles>();
+  // TODO: fill response
+  apollo::perception::PerceptionObstacle* ob =
+      response->add_perception_obstacle();
+
+  apollo::common::Point3D position;
+  position.set_x(other_ref_point.x());
+  position.set_y(other_ref_point.y());
+  position.set_z(0.0);
+  ob->mutable_position()->CopyFrom(position);
+
+  ob->set_theta(other_ref_point.heading());
+
+  // Set linear_velocity
+  apollo::common::Point3D velocity3d;
+  velocity3d.set_x(std::cos(other_ref_point.heading()) *
+                   fake_obst_config_.velocity());
+  velocity3d.set_y(std::sin(other_ref_point.heading()) *
+                   fake_obst_config_.velocity());
+  velocity3d.set_z(0);
+
+  ob->mutable_velocity()->CopyFrom(velocity3d);
+
+  // Size of obstacle bounding box.
+  ob->set_length(4);  // obstacle length.
+  ob->set_width(2);   // obstacle width.
+  ob->set_height(2);  // obstacle height.
+
+  obstacle_writer_->Write(response);
   return true;
 }
 
--- modules/fake_obstacle/fake_obstacle_component.h
+++ modules/fake_obstacle/fake_obstacle_component.h
@@ -19,6 +19,7 @@
 #include <memory>
 
 #include "cyber/cyber.h"
+#include "cyber/component/timer_component.h"
 #include "modules/canbus/proto/chassis.pb.h"
 #include "modules/common/monitor_log/monitor_log_buffer.h"
 #include "modules/localization/proto/localization.pb.h"
@@ -27,18 +28,12 @@
 #include "modules/planning/reference_line/reference_line.h"
 #include "modules/planning/reference_line/reference_line_provider.h"
 #include "modules/routing/proto/routing.pb.h"
+#include "modules/fake_obstacle/proto/fake_obstacle_conf.pb.h"
 
 namespace apollo {
 namespace fake_obstacle {
 
-/**
- * @class FakeObstacleComponent
- *
- * @brief fake obstacle module main class, it processes routing data to compute
- * a fake dynamic obstacle.
- */
-class FakeObstacleComponent final
-    : public cyber::Component<routing::RoutingResponse> {
+class FakeObstacleComponent : public apollo::cyber::TimerComponent {
  public:
   FakeObstacleComponent();
 
@@ -47,33 +42,35 @@ class FakeObstacleComponent final
  public:
   bool Init() override;
 
-  bool Proc(const std::shared_ptr<routing::RoutingResponse>& routing) override;
+  bool Proc() override;
 
  private:
   bool InitReaders();
-  bool UpdateReferenceLine(
-      const std::shared_ptr<routing::RoutingResponse>& routing);
+  bool UpdateReferenceLine();
+  bool FillPerceptionObstacles();
 
  private:
-  // std::shared_ptr<cyber::Reader<routing::RoutingResponse>> routing_reader_;
-  std::shared_ptr<cyber::Reader<localization::LocalizationEstimate>>
-      localization_reader_;
-  std::shared_ptr<cyber::Reader<canbus::Chassis>> chassis_reader_;
-  std::shared_ptr<cyber::Writer<perception::PerceptionObstacles>>
-      obstacle_writer_;
+  std::shared_ptr<cyber::Reader<routing::RoutingResponse>> routing_reader_ = nullptr;
+  std::shared_ptr<cyber::Reader<localization::LocalizationEstimate>> localization_reader_ = nullptr;
+  std::shared_ptr<cyber::Reader<canbus::Chassis>> chassis_reader_ = nullptr;
+  std::shared_ptr<cyber::Writer<perception::PerceptionObstacles>> obstacle_writer_ = nullptr;
 
+  FakeObstacleConf fake_obst_config_;
   std::mutex mutex_;
+  common::monitor::MonitorLogBuffer monitor_logger_buffer_;
 
-  // routing::RoutingResponse latest_routing_;
+  routing::RoutingResponse latest_routing_;
   canbus::Chassis latest_chassis_;
   localization::LocalizationEstimate latest_localization_;
-
-  common::monitor::MonitorLogBuffer monitor_logger_buffer_;
 
   std::unique_ptr<planning::ReferenceLineProvider> reference_line_provider_;
   std::list<planning::ReferenceLine> last_reference_lines_;
 
   const hdmap::HDMap* hdmap_ = nullptr;
+  
+  double s_init_obstacle_;
+  double t_init_obstacle_;
+  bool obstacle_has_started_;
 };
 
 CYBER_REGISTER_COMPONENT(FakeObstacleComponent)
