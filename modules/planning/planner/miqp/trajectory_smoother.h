c8447d51992b8738461f5ca2e2a2b0e15e031895
klemense1 <esterle@fortiss.org>
2021-04-21 11:28:02 +0200

    adding initial class and unit test for trajectory smoother

---
+++ modules/planning/planner/miqp/trajectory_smoother.h
@@ -0,0 +1,31 @@
+/******************************************************************************
+ * Copyright 2020 fortiss GmbH
+ * Authors: Tobias Kessler, Klemens Esterle
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *****************************************************************************/
+
+#pragma once
+
+namespace apollo {
+namespace planning {
+
+class TrajectorySmoother {
+ public:
+  explicit TrajectorySmoother();
+
+  virtual ~TrajectorySmoother() = default;
+};
+
+}  // namespace planning
+}  // namespace apollo
