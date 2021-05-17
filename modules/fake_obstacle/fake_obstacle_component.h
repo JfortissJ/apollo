bb24dbe27b2573c6838f13c84bb9c19fa7f65f31
klemense1 <esterle@fortiss.org>
2021-05-17 09:01:44 +0200

    implemented arbitrary number of fake obstacles

--- modules/fake_obstacle/fake_obstacle_component.h
+++ modules/fake_obstacle/fake_obstacle_component.h
@@ -18,21 +18,25 @@
 
 #include <memory>
 
-#include "cyber/cyber.h"
 #include "cyber/component/timer_component.h"
+#include "cyber/cyber.h"
 #include "modules/canbus/proto/chassis.pb.h"
 #include "modules/common/monitor_log/monitor_log_buffer.h"
+#include "modules/fake_obstacle/proto/fake_obstacle_conf.pb.h"
 #include "modules/localization/proto/localization.pb.h"
 #include "modules/map/hdmap/hdmap_util.h"
 #include "modules/perception/proto/perception_obstacle.pb.h"
 #include "modules/planning/reference_line/reference_line.h"
 #include "modules/planning/reference_line/reference_line_provider.h"
 #include "modules/routing/proto/routing.pb.h"
-#include "modules/fake_obstacle/proto/fake_obstacle_conf.pb.h"
 
 namespace apollo {
 namespace fake_obstacle {
 
+struct ObstacleInitState {
+  double s;
+  double t;
+};
 class FakeObstacleComponent : public apollo::cyber::TimerComponent {
  public:
   FakeObstacleComponent();
@@ -47,15 +51,18 @@ class FakeObstacleComponent : public apollo::cyber::TimerComponent {
  private:
   bool InitReaders();
   bool UpdateReferenceLine();
-  bool FillPerceptionObstacles();
+  apollo::perception::PerceptionObstacles FillPerceptionObstacles();
 
  private:
-  std::shared_ptr<cyber::Reader<routing::RoutingResponse>> routing_reader_ = nullptr;
-  std::shared_ptr<cyber::Reader<localization::LocalizationEstimate>> localization_reader_ = nullptr;
+  std::shared_ptr<cyber::Reader<routing::RoutingResponse>> routing_reader_ =
+      nullptr;
+  std::shared_ptr<cyber::Reader<localization::LocalizationEstimate>>
+      localization_reader_ = nullptr;
   std::shared_ptr<cyber::Reader<canbus::Chassis>> chassis_reader_ = nullptr;
-  std::shared_ptr<cyber::Writer<perception::PerceptionObstacles>> obstacle_writer_ = nullptr;
+  std::shared_ptr<cyber::Writer<perception::PerceptionObstacles>>
+      obstacle_writer_ = nullptr;
 
-  FakeObstacleConf fake_obst_config_;
+  FakeObstacleListConf fake_obst_list_config_;
   std::mutex mutex_;
   common::monitor::MonitorLogBuffer monitor_logger_buffer_;
 
@@ -67,10 +74,8 @@ class FakeObstacleComponent : public apollo::cyber::TimerComponent {
   std::list<planning::ReferenceLine> last_reference_lines_;
 
   const hdmap::HDMap* hdmap_ = nullptr;
-  
-  double s_init_obstacle_;
-  double t_init_obstacle_;
-  bool obstacle_has_started_;
+
+  std::map<int, ObstacleInitState> obstacles_init_states_;
 };
 
 CYBER_REGISTER_COMPONENT(FakeObstacleComponent)
