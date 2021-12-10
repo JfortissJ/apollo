24c6d8e6209e35426acca98237e363e947518bba
klemense1 <esterle@fortiss.org>
2021-12-10 12:54:43 +0100

    update bark-ml version

--- scripts/fortiss/install_bark_ml.sh
+++ scripts/fortiss/install_bark_ml.sh
@@ -1,7 +1,7 @@
 ### Update Pip Package
 python3.7 -m pip install --upgrade pip --user
 ### Install BARK ML
-python3.7 -m pip install bark-ml==0.4.26 --user
+python3.7 -m pip install bark-ml==0.4.28 --user
 
 ### Update Protobuf if you experience some weird Tensorflow Bug. (uninstalls 3.1.0 and installs 3.18.0 or newer)
 sudo pip uninstall -y protobuf
